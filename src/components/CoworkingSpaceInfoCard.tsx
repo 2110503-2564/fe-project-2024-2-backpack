6ee61c23de010f0819b68aab40c5ed72d91ce5d5
ShaneChaneShane <wannasut.chane@gmail.com>
2025-03-24 20:22:56 +0700

    feat: implementing cowokring meetingrooms

--- src/components/CoworkingSpaceInfoCard.tsx
+++ src/components/CoworkingSpaceInfoCard.tsx
@@ -1,6 +1,6 @@
 import Image from "next/image";
-export default function CoworkingSpaceCard({ id }
-    : { id: string }) {
+export default function CoworkingSpaceCard({ id, name, location, about, telephone, openinghours }
+    : { id?: string, name: string, location: string, about: string, telephone:string, openinghours: string }) {
     return (
         <div className="bg-white w-full h-auto px-12  lg:inline-flex justify-start items-center gap-7 overflow-hidden py-12">
             <div className="min-w-full lg:min-w-3/5 xl:min-w-2/5 min-h-72 h-auto lg:h-full relative rounded-[20px] overflow-hidden">
@@ -15,19 +15,19 @@ export default function CoworkingSpaceCard({ id }
             </div>
             <div className="min-w-full lg:min-w-2/5 xl:min-w-3/5 h-auto p-2.5 inline-flex flex-col justify-start items-start gap-2.5 overflow-hidden">
                 <div className="self-stretch justify-start text-black text-2xl font-bold  leading-loose">
-                    Menacing Space
+                    {name}
                 </div>
                 <div className="self-stretch min-h-7 h-auto justify-start text-neutral-600 text-base font-normal  leading-relaxed">
-                    123 Sukhumvit Road, Khlong Toei, Bangkok, Thailand
+                    {location}
                 </div>
                 <div className="self-stretch h-auto justify-start text-black text-base font-normal  leading-relaxed">
-                    Very menacing coworking space. You might encounter stand users here...  Located in the heart of the city, we provide a vibrant environment that fosters productivity, creativity, and collaboration.
+                    {about}
                 </div>
                 <div className="self-stretch justify-start text-neutral-600 text-base font-normal  leading-relaxed">
-                    Telephone: +66 2 123 4567
+                    Telephone: {telephone}
                 </div>
                 <div className="self-stretch justify-start text-neutral-600 text-base font-normal  leading-relaxed">
-                    Open-Close time: 9.00-21.00
+                    Open-Close time: {openinghours}
                 </div>
             </div>
         </div>
