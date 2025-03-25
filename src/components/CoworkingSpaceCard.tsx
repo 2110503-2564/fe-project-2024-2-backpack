9569cbc829d67213f0d48f2a9393e4b48a6d2818
ShaneChaneShane <wannasut.chane@gmail.com>
2025-03-25 13:33:40 +0700

    cowork card ui prettier

--- src/components/CoworkingSpaceCard.tsx
+++ src/components/CoworkingSpaceCard.tsx
@@ -10,8 +10,8 @@ export default function CoworkingSpaceCard({ id, name, location, about, openingh
     const searchParams = useSearchParams(); // Get the current query parameters
 
     return (
-        <div className="w-full h-auto px-5 py-5 bg-white rounded-[31px] outline outline-2 outline-offset-[-2px] outline-zinc-400 inline-flex flex-col justify-start items-start gap-4">
-            <div className="self-stretch justify-start text-black text-2xl font-bold leading-loose">
+        <div className="w-full h-auto px-5 py-5 bg-white rounded-[31px] outline outline-2 outline-offset-[-2px] outline-zinc-400 inline-flex flex-col justify-start items-start gap-1">
+            <div className="self-stretch justify-start text-center text-black text-2xl font-bold leading-loose">
                 {name}
             </div>
             <div className="w-full lg:inline-flex justify-start items-center gap-2.5">
@@ -25,14 +25,29 @@ export default function CoworkingSpaceCard({ id, name, location, about, openingh
                     </Image>
                 </div>
                 <div className="min-w-full lg:min-w-2/5 h-auto p-2.5 inline-flex flex-col justify-start items-start gap-2.5 overflow-hidden">
-                    <div className="h-1/4 self-stretch justify-start text-neutral-600 text-base font-normal   leading-tight">
+                    <div className="h-1/4 p-2 bg-stone-100 rounded-[20px] self-stretch justify-start text-neutral-600 text-base font-normal leading-tight flex items-center gap-2">
+                        <Image
+                            src="/img/locationIcon.svg"
+                            className="objectfit-auto w-5 h-5"
+                            alt="icon"
+                            width={360}
+                            height={360}
+                        />
+
                         {location}
                     </div>
-                    <div className="h-1/2 self-stretch justify-start text-black text-base font-normal   leading-tight">
+                    <div className="h-1/4 self-stretch justify-start text-neutral-600 text-base font-normal leading-tight flex items-center gap-2">
                         {about}
                     </div>
-                    <div className="h-1/4 self-stretch justify-start text-neutral-600 text-base font-normal   leading-tight">
-                        Open-Close time: {openinghours}
+                    <div className="h-1/4 p-2 gap-2.5 self-stretch justify-start text-neutral-600 text-base font-normal leading-tight flex items-center gap-2">
+                        <Image
+                            src="/img/timeIcon.svg"
+                            className="objectfit-auto w-5 h-5"
+                            alt="icon"
+                            width={360}
+                            height={360}
+                        />
+                        Opening hours:<br/> {openinghours}
                     </div>
                     <div className="h-auto w-full">
                         <Link href={{
