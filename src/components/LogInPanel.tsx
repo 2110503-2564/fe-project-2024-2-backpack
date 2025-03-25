724cc3c334f7e58c086f44928f8075e15dde90e0
MildlyBrain <maethaphan.phai@gmail.com>
2025-03-25 12:35:05 +0700

    feat: middleware with http cookie storage

--- src/components/LogInPanel.tsx
+++ src/components/LogInPanel.tsx
@@ -23,11 +23,21 @@ export default function Login() {
         const token = res.token;
         const profile = await getUserProfile(token);
         if (profile.success && "data" in profile && "role" in profile.data) {
+          // set cookie
+          await fetch("/api/login", {
+            method: "POST",
+            headers: {
+              "Content-Type": "application/json",
+            },
+            body: JSON.stringify({ token, role: profile.data.role }),
+          });
+
+          // set dispatch
           dispatch(
             setCredentials({
               token: res.token,
               role: profile.data.role as string,
-            }),
+            })
           );
           router.push("/");
           router.refresh();
--- src/middleware.ts
+++ src/middleware.ts
@@ -1,14 +1,17 @@
-// /middleware.ts
 import { NextResponse } from "next/server";
-import type { NextRequest } from "next/server";
+import { NextRequest } from "next/server";
 
 export function middleware(req: NextRequest) {
-  // const role = req.cookies.get("role")?.value; //test
-  // const isAdminRoute = req.nextUrl.pathname.startsWith("/dashboard");
-  //   console.log(role);
-  // if (isAdminRoute && role !== "admin") {
-  //   return NextResponse.redirect(new URL("/login", req.url));
-  // }
+  const { pathname } = req.nextUrl;
+  if (pathname.startsWith("/dashboard")) {
+    const token = req.cookies.get("token")?.value;
+    const role = req.cookies.get("role")?.value;
+
+    if (!token || role !== "admin") {
+      return NextResponse.redirect(new URL("/login", req.url));
+    }
+  }
+
   return NextResponse.next();
 }
 
