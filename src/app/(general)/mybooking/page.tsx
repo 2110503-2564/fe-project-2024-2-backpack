7652a99a6c1938b12470d411f76491442a1cb92c
ShaneChaneShane <wannasut.chane@gmail.com>
2025-03-25 13:33:54 +0700

    implemt profile in mybook page

--- src/app/(general)/mybooking/page.tsx
+++ src/app/(general)/mybooking/page.tsx
@@ -1,15 +1,20 @@
 "use client";
 import BookingCard from "@/components/BookingCard";
+import { getUserProfile } from "@/libs/auth";
 import { getReservations } from "@/libs/reservation";
+import { setCredentials } from "@/libs/slices/authSlice";
 import { RootState } from "@/libs/store";
 import { Reservation } from "@/types/Reservation";
+import Image from "next/image";
 import { useEffect, useState } from "react";
 import { useSelector } from "react-redux";
 export default function myBookingList() {
     const [reservations, setReservations] = useState<Reservation[] | undefined>(undefined);
     const [isLoading, setIsLoading] = useState(true);
+    const [profile, setProfile] = useState<any>(undefined);
 
     const { token } = useSelector((state: RootState) => state.auth);
+
     useEffect(() => {
         const fetchReservations = async () => {
             setIsLoading(true);
@@ -28,15 +33,64 @@ export default function myBookingList() {
         };
         fetchReservations();
     }, []);
+    useEffect(() => {
+        const fetchProfile = async () => {
+            if (!token) {
+                console.log("Failed to fetch profile");
+                setProfile(undefined);
+                setIsLoading(false);
+                return;
+            }
+            setIsLoading(true);
+            try {
+                const response = await getUserProfile(token);
+                if (
+                    response.success &&
+                    "data" in response &&
+                    "role" in response.data &&
+                    "email" in response.data &&
+                    "telephoneNumber" in response.data &&
+                    "name" in response.data
+                ) {
+                    setProfile(response.data);
+                }
+            } catch (error) {
+                console.log("Failed to fetch profile:", error);
+                setProfile(undefined);
+            }
+
+            setIsLoading(false);
+        };
+        fetchProfile();
+    }, [token]);
+
     if (isLoading) return <p>Loading...</p>;
-    if (!reservations) return;
+    if (!token) return <p>Please log in first!</p>;
     return (
-        <div className="h-auto min-h-[90vh] bg-white flex flex-col justify-start items-center">
-            <div className="w-4/5 bg-linear-to-t from-sky-500 to-white rounded-[30px] text-center justify-center text-black text-6xl font-bold leading-[162.28px]">
+        <div className=" gap-7 h-auto min-h-[90vh] bg-white flex flex-col justify-start items-center">
+            <div className="w-full p-4 bg-stone-100 flex justify-center text-center text-black">
+                <div className="w-2/3 flex items-center gap-8 p-4">
+                    <Image
+                        src={'/img/defaultProfile.svg'}
+                        alt="Profile Picture"
+                        width={120}
+                        height={120}
+                        className="rounded-full object-cover"
+                    />
+                    <div className="text-left">
+                        <h2 className="text-3xl font-bold">{profile.name}</h2>
+                        <p className="text-lg text-neutral-600">{profile.role}</p>
+                        <p className="text-lg text-neutral-600">{profile.email}</p>
+                        <p className="text-lg text-neutral-600">{profile.telephoneNumber}</p>
+                    </div>
+                </div>
+            </div>
+
+            <div className="w-4/5 p-4 bg-linear-to-t from-sky-500 to-white rounded-[10px] text-center justify-center text-black text-5xl font-bold leading-tight">
                 Your Bookings
             </div>
-            <div className="p-5 w-full">
-                {reservations.length > 0 ? (
+            <div className="px-5 w-full">
+                { reservations && reservations.length > 0 ? (
                     reservations.map((rs) => {
                         const dateObj = new Date(rs.reserveDateStart);
                         const isoString = dateObj.toISOString();
